4b991ead18a074757a518648c31740b531a48c24
Andrew Regan <andrew.regan@workday.com>
2017-12-30 11:27:21 +0000

    add setup.py

---
+++ setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup, find_packages
+
+setup(name='irish_rail',
+      version='0.0.1',
+      description='Python library to get the real-time transport information (RTPI) from Irish Rail',
+      keywords='irish rail RTPI',
+      author='Andrew Regan',
+      author_email='andrewjregan@gmail.com',
+      platforms=["any"],
+      packages=find_packages(),
+      zip_safe=False,
+      install_requires=[
+          'requests',
+          'geopy'
+      ]
+)
